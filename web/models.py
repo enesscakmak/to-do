6981fa8f2e9ba7ced17b282d51e9ebd7b1f7fd25
Enes Çakmak <cakmakkeness@gmail.com>
2023-09-13 15:00:36 +0300

    Finish By can be Null

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Date
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -5,8 +6,15 @@ db = SQLAlchemy()
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.Text)
-    priority = db.Column(db.Integer, default=1)
-    finish_by = db.Column(db.String(120), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text())
+    priority = db.Column(db.String(10), nullable=False)
+    finish_by = db.Column(Date)  # Use Date column type
     completed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, title, description, priority, finish_by, completed):
+        self.title = title
+        self.description = description
+        self.priority = priority
+        self.finish_by = finish_by
+        self.completed = completed
