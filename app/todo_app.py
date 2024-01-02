ec0d1faa950792c689a626b1c453c558d2cd759a
Enes Çakmak <cakmakkeness@gmail.com>
2024-01-02 21:29:17 +0300

    Added option to select theme with OptionMenu within app

--- app/todo_app.py
+++ app/todo_app.py
@@ -5,6 +5,7 @@ from add_task import AddTaskWindow
 from edit_task import EditTaskWindow
 import ttkthemes
 
+
 class ToDoApp:
     def __init__(self, root):
         self.root = root
@@ -17,38 +18,57 @@ class ToDoApp:
         # Load tasks from JSON file if it exists
         self.load_tasks()
 
-        # Apply a themed style
-        style = ttkthemes.ThemedStyle(root)
-        style.set_theme("arc")  # You can choose other themes like "equilux", "plastik", etc.
+        self.style = ttkthemes.ThemedStyle(root)
+        self.style.set_theme("arc")  # Starting theme(adapta looks perfect too)
 
+        # OptionMenu for selecting themes
+        self.clicked = tk.StringVar()
+        self.clicked.set("arc")
+        themes = sorted(self.style.get_themes())  # Get all themes available on pc and sort
+        themesMenu = tk.OptionMenu(root, self.clicked, *themes)
+        themesMenu.grid(row=6, column=0)
+        themesMenu.config(width=12)
+
+        # Button to apply the selected theme
+        applyThemeButton = ttk.Button(root, text="Apply Theme", command=self.change_theme)
+        applyThemeButton.grid(row=6, column=1)
+
+        self.style.configure("TLabel", foreground="black", background="#f0f0f0", font=("Helvetica", 10))
         # Create GUI elements
         self.title_label = ttk.Label(root, text="To-Do List", font=("Helvetica", 16), background="#f0f0f0")
         self.title_label.grid(row=0, column=0, columnspan=2, pady=10)
 
-        style_button = ttk.Style()
-        self.sort_by_title_button = ttk.Button(root, text="Sort by Title", command=self.sort_by_title)
+        self.sort_by_title_button = ttk.Button(root, text="Sort by Title", command=self.sort_by_title, width=22)
         self.sort_by_title_button.grid(row=1, column=0, padx=10, pady=5)
-        style_button.configure("TButton", foreground="black")
+        self.style.configure("TButton", foreground="black")
 
-        self.sort_by_priority_button = ttk.Button(root, text="Sort by Priority", command=self.sort_by_priority)
+        self.sort_by_priority_button = ttk.Button(root, text="Sort by Priority", command=self.sort_by_priority,
+                                                  width=22)
         self.sort_by_priority_button.grid(row=1, column=1, padx=10, pady=5)
 
         self.sort_by_completion_time_button = ttk.Button(root, text="Sort by Completion Time",
-                                                         command=self.sort_by_completion_time)
+                                                         command=self.sort_by_completion_time, width=22)
         self.sort_by_completion_time_button.grid(row=2, column=0, padx=10, pady=5)
 
-        self.sort_by_completed_button = ttk.Button(root, text="Sort by Completed", command=self.sort_by_completed)
+        self.sort_by_completed_button = ttk.Button(root, text="Sort by Completed", command=self.sort_by_completed,
+                                                   width=22)
         self.sort_by_completed_button.grid(row=2, column=1, padx=10, pady=5)
 
         # Create Treeview with columns
         self.task_treeview = ttk.Treeview(root,
                                           columns=("ID", "Title", "Description", "Priority", "Finish By", "Completed"),
-                                          show="headings")
+                                          show="headings", cursor="hand2")
+
         self.task_treeview.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
 
+        self.style.configure("Treeview", rowheight=25, foreground="black", font=("Helvetica", 10))
+        self.style.map("Treeview", background=[('selected', '#c88de3')], foreground=[('selected', 'black')])
+        self.style.configure("Treeview.Heading", rowheight=25, foreground="black", font=("Helvetica", 9, "bold"))
+
         # Set column headings and center text
-        for col in ["ID", "Title", "Description", "Priority", "Finish By", "Completed"]:
-            self.task_treeview.heading(col, text=col, anchor="center")
+        columns = ["ID", "Title", "Description", "Priority", "Finish By", "Completed"]
+        for col in columns:
+            self.task_treeview.heading(col, text=col, anchor="center", command=lambda c=col: self.sort_by_column(c))
             self.task_treeview.column(col, anchor="center", width=30)
             if col == "ID":
                 self.task_treeview.column(col, width=20, stretch=tk.NO)
@@ -58,16 +78,16 @@ class ToDoApp:
         root.columnconfigure(0, weight=1)
         root.columnconfigure(1, weight=1)
 
-        self.add_button = ttk.Button(root, text="Add Task", command=self.open_add_task_window)
+        self.add_button = ttk.Button(root, text="Add Task", command=self.open_add_task_window, width=22)
         self.add_button.grid(row=4, column=0, columnspan=1, pady=5)
 
-        self.edit_button = ttk.Button(root, text="Edit Task", command=self.open_edit_task_window)
+        self.edit_button = ttk.Button(root, text="Edit Task", command=self.open_edit_task_window, width=22)
         self.edit_button.grid(row=4, column=1, columnspan=1, pady=5)
 
-        self.remove_button = ttk.Button(root, text="Remove Task", command=self.remove_task)
+        self.remove_button = ttk.Button(root, text="Remove Task", command=self.remove_task, width=22)
         self.remove_button.grid(row=5, column=0, columnspan=1, pady=5)
 
-        self.complete_button = ttk.Button(root, text="Complete Task", command=self.complete_task)
+        self.complete_button = ttk.Button(root, text="Complete Task", command=self.complete_task, width=22)
         self.complete_button.grid(row=5, column=1, columnspan=1, pady=5)
 
         # Save tasks to JSON file when the application is closed
@@ -77,6 +97,10 @@ class ToDoApp:
         # Make the GUI responsive to window resizing
         root.rowconfigure(0, weight=1)
         root.columnconfigure(0, weight=1)
+
+    def change_theme(self):
+        style = ttkthemes.ThemedStyle(root)
+        style.theme_use(self.clicked.get())
 
     def sort_by_title(self):
         self.tasks.sort(key=lambda task: task["title"])
@@ -92,6 +116,14 @@ class ToDoApp:
 
     def sort_by_completed(self):
         self.tasks.sort(key=lambda task: task["completed"])
+        self.update_task_list()
+
+    def sort_by_column(self, column):
+        if column == "ID":
+            return
+        else:
+            column_key = column.replace(" ", "_").lower()
+        self.tasks.sort(key=lambda task: task[column_key])
         self.update_task_list()
 
     def add_task(self, title, description, priority, finish_by, completed):
@@ -123,6 +155,7 @@ class ToDoApp:
                 self.tasks[index]["completed"] = True
                 self.update_task_list()
                 self.save_tasks()
+
     def center_window(self):
         # Get window width and height
         window_width = self.root.winfo_reqwidth()
@@ -133,9 +166,9 @@ class ToDoApp:
         screen_height = self.root.winfo_screenheight()
 
         # Calculate position of window's top-left corner
-        # 2.5 and 2.2 is because it was in middle of screen like this (for 1920x1080)
+        # 3 and 2.2 is because it was in middle of screen like this (for 1920x1080)
         # but i don't know for sure if it will be the same for every screen
-        position_top = int(screen_height / 2.5 - window_height / 2)
+        position_top = int(screen_height / 3 - window_height / 2)
         position_right = int(screen_width / 2.2 - window_width / 2)
 
         # Set the position of the window
@@ -150,7 +183,7 @@ class ToDoApp:
         for task_index, task in enumerate(self.tasks):
             # Use task_index as an identifier for the task
             self.task_treeview.insert("", tk.END, values=(
-            task_index, task["title"], task["description"], task["priority"], task["finish_by"], task["completed"]))
+                task_index, task["title"], task["description"], task["priority"], task["finish_by"], task["completed"]))
 
     def save_tasks(self):
         with open("tasks.json", "w") as f:
